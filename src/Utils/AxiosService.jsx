0eee2baa940a748adfbcbfb2a320b8854bd7a212
Dharanish3 <dharanishsk9698@gmail.com>
2024-02-03 15:36:24 +0530

    Api service

--- src/Utils/AxiosService.jsx
+++ src/Utils/AxiosService.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const AxiosService = axios.create({
-    baseURL: 'http://localhost:4000/',
+    baseURL: 'https://auth-wxak.onrender.com/',
    
     headers: {'content-type': 'application/json'}
   });
